cb1dd20736b66e81a925f778ac9b3354a0ff173e
Mahir-Rao <mahirrao010@gmail.com>
2025-12-14 23:03:13 +0530

    Final

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Hey everyone :) Our LAMP CI/CD Pipeline Working 🚀</h1>";
+echo "<h1>Hey everyone :), Our LAMP CI/CD Pipeline Working 🚀</h1>";
 echo "<p>Deployed via Jenkins + GitHub Webhooks</p>";
 echo "<p>Timestamp: " . date("Y-m-d H:i:s") . "</p>";
 ?>
